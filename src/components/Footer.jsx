be6791dd7939a9d2fe6309891be88507dd473267
imamul <imamulrakib918273@gmail.com>
2025-07-27 11:34:25 +0600

    version2

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -4,7 +4,7 @@ const Footer = () => {
   return (
     <footer className="py-5 bg-gray-200">
       <div className="container">
-        <h1>Footer</h1>
+        <p>@2025 Imamul | All Rights reserved</p>
       </div>
     </footer>
   );
