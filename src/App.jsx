5d0c7361d68c36e074f8ac1fbe140f7106c857b2
imamul <imamulrakib918273@gmail.com>
2025-07-27 11:26:25 +0600

    version1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import Footer from "./components/Footer";
 import Header from "./components/Header";
 
 const App = () => {
@@ -68,6 +69,7 @@ const App = () => {
           </p>
         </div>
       </section>
+      <Footer />
     </>
   );
 };
---
+++ src/components/Footer.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Footer = () => {
+  return (
+    <footer className="py-5 bg-gray-200">
+      <div className="container">
+        <h1>Footer</h1>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
